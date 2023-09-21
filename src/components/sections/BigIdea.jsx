652047a8e3f8a20b3abb280c1c12410ad5c0e662
Levi <francisking151@gmail.com>
2023-09-21 15:40:18 +0100

    feat: add big idea section

---
+++ src/components/sections/BigIdea.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const BigIdea = () => {
+  return (
+    <div>
+        <section className='flex flex-col items-center lg:flex-row lg:justify-evenly text-center border-b-[0.1px] border-opacity-20 border-b-white lg:items-center lg:max-w-[1500px] mx-auto py-8 px-8'>
+
+            <div className='py-2 lg:px-8 lg:min-w-[500px]'>
+                <img src='/the-big-idea.png' alt='the big idea' />
+            </div>
+            <div className='lg:px-24'>
+                <div className=' lg:max-w-[398px]'>
+                    <h2 className='text-white w-full text-center lg:text-left lg:text-[32px] font-semibold'>Introduction to getlinked <span className='text-[#D434FE]'>tech Hackathon 1.0</span></h2>
+                </div>
+                
+                <div className='lg:max-w-[535px]'>
+                    <p className='text-white text-center lg:text-left'>
+                    Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as 
+    day: to shape the future. Whether you're 
+    a coding genius, a design maverick, or a 
+    concept wizard, you'll have the chance to transform your ideas into reality. Solving 
+    real-world problems, pushing the boundaries
+    of technology, and creating solutions that can change the world, that's what we're all about!
+                    </p>
+                </div>
+            </div>
+
+        </section>
+    </div>
+  )
+}
+
+export default BigIdea
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/sections/Hero'
 import { Rules } from '../Components/Sections/Rules'
+import BigIdea from '../components/sections/BigIdea'
 
 const Homepage = () => {
   return (
     <>
         <Navbar />
         <Hero />
+        <BigIdea />
         <Rules />
     </>
   )
