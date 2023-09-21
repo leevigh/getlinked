fee2494d9c5870aa8e7f909e8b617f9dd6fe5004
Levi <francisking151@gmail.com>
2023-09-21 13:18:47 +0100

    feat: add hero section

---
+++ src/components/Underline.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Underline = () => {
+  return (
+    <>
+    {/* width="117" height="11" */}
+            <svg className='w-[102px] h-[11px] lg:hidden' viewBox="0 0 117 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path id="Vector 4" d="M1 9C20.2424 3.71764 70.7273 -3.67768 116 9" stroke="#FF26B9" stroke-width="3"/>
+        </svg>
+
+        <svg className='hidden lg:block' width="255" height="17" viewBox="0 0 255 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path id="Vector 4" d="M1 14.043C43.3333 5.7097 154.4 -5.95697 254 14.043" stroke="#FF26B9" stroke-width="5"/>
+</svg>
+
+
+    </>
+  )
+}
+
+export default Underline
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/sections/Hero'
+
+const Homepage = () => {
+  return (
+    <>
+        <Navbar />
+        <Hero />
+    </>
+  )
+}
+
+export default Homepage
