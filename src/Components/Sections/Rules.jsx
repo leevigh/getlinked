6ca72c65d80bedaba69550890da9b9a283f8b880
james2000-design <65454097+james2000-design@users.noreply.github.com>
2023-09-21 12:21:44 +0100

    This is the rule section

---
+++ src/Components/Sections/Rules.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import read from "../../assets/read.png"
+
+export const Rules = () => {
+  return (
+    <div className='bg-custom-bg-color'>
+
+
+        <div className=' flex flex-wrap-reverse lg:flex-nowrap justify-center items-center px-5 '>
+
+            <div className=' text-center font-sans  px-14 lg:w-1/2 lg:text-left lg:ml-[3rem]  lg:mr-[2rem] lg:pr-[1.5rem]'>
+                <p  className='text-xl font-bold font-ClashDisplay'>
+                    <span className='text-white '>Rules and</span><br/>
+                    <span className='text-purple-700'>Guidelines</span>
+                </p>
+
+                <p className='text-white text-[14px] p-4 lg:p-0'>
+                    Our tech hackathon is a melting pot of visionaries, and its purpose is as
+                    clear as day: to shape the future. Whether you're a coding genius, a 
+                    design maverick, or a concept wizard, you'll have the chance to transform 
+                    your ideas into reality. Solving real-world problems, pushing the boundaries
+                    of technology, and creating solutions that can change the world,
+                    that's what we're all about!
+                </p>
+
+            </div>
+
+            <div className='flex justify-center '>
+                <img src={read} alt="IMG" className=' h-200 w-200 items-center '/>
+            </div>
+        </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-bg-color': '#140D27',
+      },
+     
+    },
   },
   plugins: [],
 }
+
